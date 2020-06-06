dd5c3762de3423140f1c5c58dd17aa1308b4ccfd
Aditya Sovani <adityasovani44111@gmail.com>
2020-06-06 16:17:04 +0530

    Removed unnecessary

--- src/main/java/com/cg/controller/TaskUpdateController.java
+++ src/main/java/com/cg/controller/TaskUpdateController.java
@@ -3,7 +3,6 @@ package com.cg.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.bean.Task;
 import com.cg.dao.TaskUpdateServiceProxy;
-
-import io.swagger.annotations.Api;
 
 @RestController
 @CrossOrigin
