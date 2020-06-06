301c3264e6d9c2574946d6ed63ad59db9c8e3b91
Aditya Sovani <adityasovani44111@gmail.com>
2020-06-06 16:27:28 +0530

    Proxy fix #4

--- src/main/java/com/cg/controller/TaskUpdateController.java
+++ src/main/java/com/cg/controller/TaskUpdateController.java
@@ -3,6 +3,7 @@ package com.cg.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +31,7 @@ public class TaskUpdateController {
 	}
 	
 	@DeleteMapping("deleteTask/{taskId}")
-	public void deleteTask(@RequestParam int taskId) {
+	public void deleteTask(@PathVariable int taskId) {
 		 proxy.deleteTask(taskId);
 	}
 }
